b19aa097a03b90726c3aa45be1a372968175d802
small-writer <945304243@qq.com>
2020-02-11 14:00:30 +0800

    number7

--- number7/number7.py
+++ number7/number7.py
@@ -1,9 +1,12 @@
-num=int(input('n='))
+num=input('请输入正数：')
 i=1
-while i<=num:
-    if '7' in str(i) or i%7==0:
-        print('pass')
+if num.isdigit() and int(num)>0:
+    while i<=int(num):
+        if '7' in str(i) or i%7==0:
+            print('pass')
 
-    else:
-        print(i)
-    i+=1
+        else:
+            print(i)
+        i+=1
+else:
+    print('请输入正数')

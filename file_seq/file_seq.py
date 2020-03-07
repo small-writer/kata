29640e914bf62c8293cac40e78ab56ed578cd41f
small-writer <945304243@qq.com>
2020-03-07 18:06:56 +0800

    修改

--- file_seq/file_seq.py
+++ file_seq/file_seq.py
@@ -1,11 +1,7 @@
 import random
-def num_sort(list):
-    for i in range(len(list) - 1):
-        for j in range(len(list) - 1):
-            if list[j] > list[j + 1]:
-                list[j], list[j + 1] = list[j + 1], list[j]
+import publishs
 
-def fileAccessor(filename,seq):
+def file_accessor(filename,seq):
     f=open(filename, 'w+')
     f.write(','.join(str(i) for i in seq))
     f.close()
@@ -13,7 +9,7 @@ def fileAccessor(filename,seq):
 seq=list(range(3000))
 new_seq=random.sample(seq,random.randint(1,100))
 
-fileAccessor('file',new_seq)
+file_accessor('file',new_seq)
 
-num_sort(new_seq)
-fileAccessor('new_file',new_seq)
+publishs.bublle_sort(new_seq)
+file_accessor('new_file',new_seq)

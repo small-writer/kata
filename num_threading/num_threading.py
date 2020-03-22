5c9eb90d2cb061d252a77289a0d3013d3551aa74
small-writer <945304243@qq.com>
2020-03-22 10:18:30 +0800

    修改

--- num_threading/num_threading.py
+++ num_threading/num_threading.py
@@ -1,27 +1,31 @@
+import os
 import threading
 import random
-import publishs
-import os
+import sys
+sys.path.append('..')
+from publishs import mod
+
+
 #生成随机数组文件
 def generate_orig(data_size,list):
     while data_size < file_size:
         list.append(random.randint(1, 9))
-        publishs.file_accessor('original_file', list)
+        mod.file_accessor('original_file', list)
         data_size = os.path.getsize('original_file')
     print(os.path.getsize('original_file'))
 #正序
 def ascending_file():
-    list1=list
-    publishs.bublle_sort(list1)
-    publishs.file_accessor('ascend_file',list1)
+    list1=list.copy()
+    mod.bublle_sort(list1)
+    mod.file_accessor('ascend_file',list1)
     data_size = os.path.getsize('ascend_file')
     print(os.path.getsize('ascend_file'))
     print('thread1 pass')
 #逆序
 def descending_file():
-    list2=list
-    publishs.insertion_sort(list2)
-    publishs.file_accessor('descend_file',list2)
+    list2=list.copy()
+    mod.insertion_sort(list2)
+    mod.file_accessor('descend_file',list2)
     data_size = os.path.getsize('descend_file')
     print(os.path.getsize('descend_file'))
     print('thread2 pass')
@@ -35,7 +39,7 @@ def main():
 input_size=input('请输入文件大小：')
 list = []
 data_size = 0
-if publishs.positive_num(input_size):
+if mod.positive_num(input_size):
     file_size = int(input_size)
     generate_orig(data_size,list)
     main()
